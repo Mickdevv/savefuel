4552da4e02707ee6e455a4599a3103a2b19a322d
Arch desktop <michael@devereux.fm>
2026-06-05 19:07:10 +0200

    Four guarantees done, technical page done

---
+++ frontend/src/components/DocumentCard.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+const props = defineProps<{
+  properties: { icon: string, title: string, description: string, link: string, type: string }
+}>()
+</script>
+
+
+<template>
+  <a target="_blank" :href="$t(`links.${properties.type}.${properties.link}`)">
+    <i :class="`pi pi-${properties.icon}`"></i>
+    <div>
+      <div class="title">{{ properties.title }}</div>
+      <div class="description">
+        {{ properties.description }}
+      </div>
+    </div>
+  </a>
+</template>
+
+<style scoped>
+.title {
+  text-align: left;
+  font-weight: 600;
+  font-size: 15px;
+  margin-bottom: 4px;
+  color: var(--primary-color-gold)
+}
+
+i {
+  color: var(--primary-color-gold)
+}
+
+.description {
+  color: var(--grey);
+  text-align: left;
+  margin-bottom: 20px;
+  flex: 1;
+  font-size: 13px;
+  line-height: 1.4;
+}
+
+a {
+  display: flex;
+  gap: 12px;
+  padding: 16px;
+  background: rgb(244, 244, 244);
+  border-radius: 8px;
+  border: 1px solid rgb(232, 232, 232);
+  text-decoration: none;
+  color: rgb(28, 28, 28);
+  transition: border-color 150ms, box-shadow 150ms;
+  box-shadow: none;
+}
+
+a:hover {
+  border: 1px solid var(--primary-color-gold);
+}
+</style>
--- frontend/src/views/TechnicalView.vue
+++ frontend/src/views/TechnicalView.vue
@@ -2,7 +2,10 @@
 import Button from 'primevue/button'
 import { useCurrentPageStore } from '@/stores/current-page';
 import { useRouter } from 'vue-router';
+import DocumentCard from '@/components/DocumentCard.vue';
+import { useI18n } from 'vue-i18n';
 
+const { tm } = useI18n()
 const router = useRouter()
 const currentPageStore = useCurrentPageStore()
 currentPageStore.setCurrentPage("technical")
@@ -12,123 +15,124 @@ currentPageStore.setCurrentPage("technical")
   <div class="page-container">
 
     <div class="paragraphs">
-      <!-- <h2>{{ $t('pages.technical.history.title') }}</h2> -->
+      <h2>{{ $t('pages.technical.history.title') }}</h2>
 
+      <div class="section-divider"></div>
       <p>{{ $t('pages.technical.history.p1') }}</p>
       <p>{{ $t('pages.technical.history.p2') }}</p>
       <p>{{ $t('pages.technical.history.p3') }}</p>
       <p>{{ $t('pages.technical.history.p4') }}</p>
+      <p>{{ $t('pages.technical.history.p5') }}</p>
 
       <h2>{{ $t('pages.technical.technical-documents.title') }}</h2>
+      <div class="section-divider"></div>
+      <div class="subtitle">{{ $t('pages.technical.technical-documents.subtitle') }}</div>
+      <!---->
+      <!-- <p> -->
+      <!--   {{ $t('pages.technical.technical-documents.video-trial-text') }} -->
+      <!--   <a :href="$t('links.videos.long-fuel-ox-test')" target="_blank" rel="noopener noreferrer"> -->
+      <!--     {{ $t('pages.technical.technical-documents.video-trial-link') }} -->
+      <!--   </a> -->
+      <!-- </p> -->
+      <!---->
+      <!-- <p> -->
+      <!--   <a target="_blank" :href="$t('links.documents.product-information')"> -->
+      <!--     {{ $t('pages.technical.technical-documents.p1_1') }} -->
+      <!--   </a> -->
+      <!--   - -->
+      <!--   {{ $t('pages.technical.technical-documents.p1_2') }} -->
+      <!-- </p> -->
+      <!---->
+      <!-- <p> -->
+      <!--   <a target="_blank" :href="$t('links.documents.fo-white-paper')"> -->
+      <!--     {{ $t('pages.technical.technical-documents.p2_1') }} -->
+      <!--   </a> -->
+      <!--   - -->
+      <!--   {{ $t('pages.technical.technical-documents.p2_2') }} -->
+      <!-- </p> -->
+      <!---->
+      <!-- <p> -->
+      <!--   <a target="_blank" :href="$t('links.documents.faq')"> -->
+      <!--     {{ $t('pages.technical.technical-documents.p3_1') }} -->
+      <!--   </a> -->
+      <!--   - -->
+      <!--   {{ $t('pages.technical.technical-documents.p3_2') }} -->
+      <!---->
+      <!--   <a href="mailto:contact@save-fuel.eu?subject=FAQ"> -->
+      <!--     contact@save-fuel.eu -->
+      <!--   </a> -->
+      <!-- </p> -->
+      <!---->
+      <!-- <p> -->
+      <!--   <a target="_blank" :href="$t('links.documents.gnr-compared-to-EN590')"> -->
+      <!--     {{ $t('pages.technical.technical-documents.p4_1') }} -->
+      <!--   </a> -->
+      <!--   - -->
+      <!--   {{ $t('pages.technical.technical-documents.p4_2') }} -->
+      <!-- </p> -->
+      <!---->
+      <!-- <p> -->
+      <!--   <a target="_blank" :href="$t('links.documents.fo-MSDS')"> -->
+      <!--     {{ $t('pages.technical.technical-documents.p5_1') }} -->
+      <!--   </a> -->
+      <!--   - -->
+      <!--   {{ $t('pages.technical.technical-documents.p5_2') }} -->
+      <!-- </p> -->
+      <!---->
+      <!-- <p> -->
+      <!--   <a target="_blank" :href="$t('links.documents.intertek-EN590-test')"> -->
+      <!--     {{ $t('pages.technical.technical-documents.p6_1') }} -->
+      <!--   </a> -->
+      <!--   - -->
+      <!--   {{ $t('pages.technical.technical-documents.p6_2') }} -->
+      <!-- </p> -->
+      <!---->
+      <!-- <p> -->
+      <!--   <a target="_blank" :href="$t('links.documents.ASTM-D975-test')"> -->
+      <!--     {{ $t('pages.technical.technical-documents.p7_1') }} -->
+      <!--   </a> -->
+      <!--   - -->
+      <!--   {{ $t('pages.technical.technical-documents.p7_2') }} -->
+      <!-- </p> -->
+      <!---->
+      <!-- <p> -->
+      <!--   <a target="_blank" :href="$t('links.documents.fo-guarantee')"> -->
+      <!--     {{ $t('pages.technical.technical-documents.p8_1') }} -->
+      <!--   </a> -->
+      <!--   - -->
+      <!--   {{ $t('pages.technical.technical-documents.p8_2') }} -->
+      <!-- </p> -->
+      <!---->
 
-      <p>
-        {{ $t('pages.technical.technical-documents.video-trial-text') }}
-        <a
-          :href="$t('links.videos.long-fuel-ox-test')"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {{ $t('pages.technical.technical-documents.video-trial-link') }}
-        </a>
-      </p>
+      <div class="technical-documents">
 
-      <p>
-        <a target="_blank" :href="$t('links.documents.product-information')">
-          {{ $t('pages.technical.technical-documents.p1_1') }}
-        </a>
-        -
-        {{ $t('pages.technical.technical-documents.p1_2') }}
-      </p>
 
-      <p>
-        <a target="_blank" :href="$t('links.documents.fo-white-paper')">
-          {{ $t('pages.technical.technical-documents.p2_1') }}
-        </a>
-        -
-        {{ $t('pages.technical.technical-documents.p2_2') }}
-      </p>
+        <div class="document-links">
 
-      <p>
-        <a target="_blank" :href="$t('links.documents.faq')">
-          {{ $t('pages.technical.technical-documents.p3_1') }}
-        </a>
-        -
-        {{ $t('pages.technical.technical-documents.p3_2') }}
-
-        <a href="mailto:contact@save-fuel.eu?subject=FAQ">
-          contact@save-fuel.eu
-        </a>
-      </p>
-
-      <p>
-        <a target="_blank" :href="$t('links.documents.gnr-compared-to-EN590')">
-          {{ $t('pages.technical.technical-documents.p4_1') }}
-        </a>
-        -
-        {{ $t('pages.technical.technical-documents.p4_2') }}
-      </p>
-
-      <p>
-        <a target="_blank" :href="$t('links.documents.fo-MSDS')">
-          {{ $t('pages.technical.technical-documents.p5_1') }}
-        </a>
-        -
-        {{ $t('pages.technical.technical-documents.p5_2') }}
-      </p>
-
-      <p>
-        <a target="_blank" :href="$t('links.documents.intertek-EN590-test')">
-          {{ $t('pages.technical.technical-documents.p6_1') }}
-        </a>
-        -
-        {{ $t('pages.technical.technical-documents.p6_2') }}
-      </p>
-
-      <p>
-        <a target="_blank" :href="$t('links.documents.ASTM-D975-test')">
-          {{ $t('pages.technical.technical-documents.p7_1') }}
-        </a>
-        -
-        {{ $t('pages.technical.technical-documents.p7_2') }}
-      </p>
-
-      <p>
-        <a target="_blank" :href="$t('links.documents.fo-guarantee')">
-          {{ $t('pages.technical.technical-documents.p8_1') }}
-        </a>
-        -
-        {{ $t('pages.technical.technical-documents.p8_2') }}
-      </p>
-    </div>
-
-    <div class="right-column">
-      <div class="images">
-        <img
-          src="../assets/Fuel_molecules_polymerised.png"
-          style="border-radius: 5px; width: 100%"
-          alt=""
-        />
-
-        <img
-          src="../assets/Fuel_molecules_dispersed.png"
-          style="border-radius: 5px; width: 100%"
-          alt=""
-        />
-      </div>
-
-      <div class="buttons-bar">
-        <Button
-          class="primary-button"
-          @click="router.push('/about')"
-          :label="$t('pages.technical.about-button')"
-        />
+          <DocumentCard :properties="d" v-for="d in tm('components.document-cards')" />
+        </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <style scoped>
+.subtitle {
+  color: var(--grey);
+  margin-bottom: 1.5rem;
+}
+
+.section-divider {
+  margin-bottom: 1.5rem;
+}
+
+.document-links {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  gap: 16px;
+  margin-bottom: 40px;
+}
+
 .right-column {
   margin-left: 1rem;
 }
@@ -139,9 +143,9 @@ currentPageStore.setCurrentPage("technical")
 }
 
 .page-container {
+  justify-content: center;
   display: flex;
   flex-wrap: wrap;
-  align-items: flex-start;
 }
 
 .paragraphs {
