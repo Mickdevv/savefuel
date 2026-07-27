be05b16f4fb2ceb9807c84a13fabeacaee079969
stevedevereux <steve@devereux.fm>
2026-07-27 19:13:26 +0200

    Worked on agreed changes.

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
-import { useCurrentPageStore } from '@/stores/current-page';
-import Button from 'primevue/button'
-import { useRouter } from 'vue-router';
+import { useCurrentPageStore } from '@/stores/current-page'
+import { useRouter } from 'vue-router'
 
 const router = useRouter()
 const currentPageStore = useCurrentPageStore()
+
 currentPageStore.setCurrentPage('about')
 </script>
+
 <template>
   <div class="page-container">
     <h2>{{ $t('pages.about.title') }}</h2>
+
     <div class="section-divider"></div>
+
     <p>{{ $t('pages.about.p1') }}</p>
     <p>{{ $t('pages.about.p2') }}</p>
     <p>{{ $t('pages.about.p3') }}</p>
@@ -18,23 +21,30 @@ currentPageStore.setCurrentPage('about')
     <p>{{ $t('pages.about.p5') }}</p>
     <p>{{ $t('pages.about.p6') }}</p>
     <p>{{ $t('pages.about.p7') }}</p>
-    <!-- <div class="subtitle-card"> -->
-    <!--   <h3> {{ $t('pages.about.contact.title') }} </h3> -->
-    <!--   <p> {{ $t('pages.about.contact.phone.label') }}{{ $t('pages.about.contact.phone.number') }} </p> -->
-    <!--   <p> contact@save-fuel.eu </p> -->
-    <!-- </div> -->
+
+    <button
+      class="free-trial-button button primary-button"
+      severity="warning"
+      @click="router.push('/free-trial-procedure')"
+    >
+      {{ $t('pages.how-fo-cuts-costs.explore-a-free-trial-button') }}
+    </button>
   </div>
 </template>
+
 <style scoped>
-.page-container {}
+.free-trial-button {
+  display: block;
+  width: fit-content;
+  max-width: 100%;
+  margin: 2rem auto 0;
+}
 
 .right-container .left-container {
   width: 100%;
 }
 
 @media (min-width: 1080px) {
-  .page-container {}
-
   .right-container {
     max-width: 25%;
   }
